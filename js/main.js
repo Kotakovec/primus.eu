a199a21004680ac0f4c298d21e59ac17a931b676
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-08-15 22:31:31 +0200

    GIANT MASSIVE page update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 // jednoduchá data struktura pro články (nahraď reálnými daty)
 const posts = [
-  { id:1, title:"VYHLÁŠENÍ - Squid Game", date:"8.8.2025", excerpt:"Ve středu proběhl SquidGame event S2...", image:"linear-gradient(180deg,#8a3b2e,#5a2a20)"},
-  { id:2, title:"SQUID GAME S2", date:"4.8.2025", excerpt:"SQUID GAME SEZÓNA 2... milionový jackpot", image:"linear-gradient(180deg,#b26f2a,#6b3b2a)"},
-  { id:3, title:"RESOURCE PACK", date:"26.7.2025", excerpt:"Nový systém craftingových bloků a nábytku...", image:"linear-gradient(180deg,#274b3b,#143527)"},
-  { id:4, title:"AQUARYON", date:"14.7.2025", excerpt:"Summer draK Aquaryon obsadil End arénu...", image:"linear-gradient(180deg,#3b2a5a,#1a0f2a)"},
-  { id:5, title:"BATTLE PASS", date:"30.6.2025", excerpt:"BattlePass 4. sezóna od 17.7.2025...", image:"linear-gradient(180deg,#6b2a2a,#3b1a1a)"},
-  { id:6, title:"Novinky & Eventy", date:"29.7.2025", excerpt:"Nová GACHA sezóna - SUMMER", image:"linear-gradient(180deg,#3b5a2a,#1a2b18)"},
+  { id:1, title:"SERVER SE PŘIPRAVUJE", date:"15.8.2025", excerpt:"Připravujeme pro vás server Primus - Next Level Minecraftu", image:"linear-gradient(180deg,#b26f2a,#6b3b2a)"}
 ];
 
 function createPostCard(post){
---
+++ js/vars.js
@@ -0,0 +1,10 @@
+document.getElementById('navb').innerHTML = `
+        <a class="menu-item active" href="#">Hlavní Stránka</a>
+        <a class="menu-item" href="/at">AdminTeam</a>
+        <a class="menu-item" href="#">Store</a>
+        <a class="menu-item" href="#">Pravidla</a>
+        <a class="menu-item" href="#">Wiki</a>
+        <a class="menu-item" href="#">Spolupráce</a>
+        <a class="menu-item" href="#">Nábory</a>
+        <a class="menu-item" href="#">Statistiky</a>
+`;
