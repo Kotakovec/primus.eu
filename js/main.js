c0863e8bd8c56a0d00685d6a7f79a48065c2edb9
Kotakovec <kotakjakub@gmail.com>
2025-08-16 17:04:33 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // jednoduchá data struktura pro články (nahraď reálnými daty)
 const posts = [
   { id:1, title:"SERVER SE PŘIPRAVUJE", date:"15.8.2025", excerpt:"Připravujeme pro vás server Primus - Next Level Minecraftu", image:"linear-gradient(180deg,#b26f2a,#6b3b2a)"},
-  { id:2, title:"Se serverem nám pomáhá FrCreator", date:"16.8.2025", excerpt:"Lidi z FrCreatoru: Koťák a MrTomiCZ", image:"linear-gradient(180deg,#b26f2a,#6b3b2a)"}
+  { id:2, title:"Se serverem nám pomáhá FrCreator", date:"16.8.2025", excerpt:"Lidi z FrCreatoru: Koťák a MrTomiCZ", image:"linear-gradient(180deg,#9B00FF,#7D00D1)"}
 ];
 
 function createPostCard(post){
